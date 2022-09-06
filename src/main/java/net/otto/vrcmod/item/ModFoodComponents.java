54ee2fdf26535271dd099fddd9acfd9d6eb213e9
zGMGT <zgmgtvz@gmail.com>
2022-09-06 09:46:35 +0300

    Add normal food points and saturation

--- src/main/java/net/otto/vrcmod/item/ModFoodComponents.java
+++ src/main/java/net/otto/vrcmod/item/ModFoodComponents.java
@@ -4,21 +4,21 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent SWEET_JUICE =
-            new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
+            new FoodComponent.Builder().hunger(3).saturationModifier(0.6f).build();
     public static final FoodComponent SWEETEST_JUICE =
-            new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build();
+            new FoodComponent.Builder().hunger(6).saturationModifier(1f).build();
     public static final FoodComponent CANDY_POP =
-            new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
+            new FoodComponent.Builder().hunger(4).saturationModifier(1.8f).build();
     public static final FoodComponent CHOKAK =
-            new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
+            new FoodComponent.Builder().hunger(11).saturationModifier(2.1f).build();
     public static final FoodComponent FUNNY_JACK =
-            new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
+            new FoodComponent.Builder().hunger(7).saturationModifier(2.6f).build();
     public static final FoodComponent HEARTLESS =
-            new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
+            new FoodComponent.Builder().hunger(12).saturationModifier(2.1f).build();
     public static final FoodComponent NOT_CYAN =
-            new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
+            new FoodComponent.Builder().hunger(6).saturationModifier(3f).build();
     public static final FoodComponent RETAW =
-            new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
+            new FoodComponent.Builder().hunger(3).saturationModifier(0.6f).build();
     public static final FoodComponent TRIPLE_W =
-            new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
+            new FoodComponent.Builder().hunger(4).saturationModifier(2.4f).build();
 }
