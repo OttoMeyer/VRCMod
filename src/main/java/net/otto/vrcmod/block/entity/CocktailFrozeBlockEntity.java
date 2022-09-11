ac08bc65f3c29e56b728d2bb3c237c0495c6224b
zGMGT <zgmgtvz@gmail.com>
2022-09-12 01:06:12 +0300

    FrozeBarrel

--- src/main/java/net/otto/vrcmod/block/entity/CocktailFrozeBlockEntity.java
+++ src/main/java/net/otto/vrcmod/block/entity/CocktailFrozeBlockEntity.java
@@ -1,6 +1,10 @@
 package net.otto.vrcmod.block.entity;
 
+import com.google.common.collect.Maps;
+import net.minecraft.SharedConstants;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.block.entity.BarrelBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
@@ -8,14 +12,21 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.tag.TagKey;
 import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.World;
 import net.otto.vrcmod.block.custom.FrozeBarrel;
 import net.otto.vrcmod.item.ModItems;
@@ -23,6 +34,8 @@ import net.otto.vrcmod.recipe.CocktailFrozeRecipe;
 import net.otto.vrcmod.screen.CocktailFrozeScreenHandler;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class CocktailFrozeBlockEntity extends BlockEntity implements NamedScreenHandlerFactory, ImplementedInventory {
@@ -30,6 +43,7 @@ public class CocktailFrozeBlockEntity extends BlockEntity implements NamedScreen
     protected final PropertyDelegate propertyDelegate;
     private int progress = 0;
     private int maxProgress = 72;
+    private int frozeTime = 0;
 
 
     private final DefaultedList<ItemStack> inventory =
@@ -42,6 +56,7 @@ public class CocktailFrozeBlockEntity extends BlockEntity implements NamedScreen
                 switch (index) {
                     case 0: return CocktailFrozeBlockEntity.this.progress;
                     case 1: return CocktailFrozeBlockEntity.this.maxProgress;
+                    case 2: return CocktailFrozeBlockEntity.this.frozeTime;
                     default: return 0;
                 }
             }
@@ -50,6 +65,7 @@ public class CocktailFrozeBlockEntity extends BlockEntity implements NamedScreen
                 switch(index) {
                     case 0: CocktailFrozeBlockEntity.this.progress = value; break;
                     case 1: CocktailFrozeBlockEntity.this.maxProgress = value; break;
+                    case 2: CocktailFrozeBlockEntity.this.frozeTime = value; break;
                 }
             }
 
@@ -57,6 +73,21 @@ public class CocktailFrozeBlockEntity extends BlockEntity implements NamedScreen
                 return 2;
             }
         };
+    }
+
+    public static Map<Item, Integer> createFuelTimeMap() {
+        LinkedHashMap<Item, Integer> map = Maps.newLinkedHashMap();
+        CocktailFrozeBlockEntity.addFuel(map, Items.SNOWBALL, 20);
+        return map;
+    }
+
+    private static void addFuel(Map<Item, Integer> fuelTimes, ItemConvertible item, int fuelTime) {
+        Item item2 = item.asItem();
+        fuelTimes.put(item2, fuelTime);
+    }
+
+    private boolean isFrozing() {
+        return this.frozeTime > 0;
     }
 
     @Override
@@ -81,27 +112,45 @@ public class CocktailFrozeBlockEntity extends BlockEntity implements NamedScreen
     protected void writeNbt(NbtCompound nbt) {
         super.writeNbt(nbt);
         Inventories.writeNbt(nbt, inventory);
-        nbt.putInt("coctail_froze_barrel.progress", progress);
+        nbt.putInt("cocktail_froze_barrel.progress", progress);
+        nbt.putInt("cocktail_froze_barrel.frozeTime", frozeTime);
     }
 
     @Override
     public void readNbt(NbtCompound nbt) {
         Inventories.readNbt(nbt, inventory);
         super.readNbt(nbt);
-        progress = nbt.getInt("coctail_froze_barrel.progress");
+        progress = nbt.getInt("cocktail_froze_barrel.progress");
+        frozeTime = nbt.getInt("cocktail_froze_barrel.frozeTime");
     }
 
+    @Override
+    public boolean canInsert(int slot, ItemStack stack, @Nullable Direction side) {
+
+        return (side == Direction.UP && slot == 1) ||
+                ((side == Direction.EAST || side == Direction.WEST || side == Direction.NORTH || side == Direction.SOUTH) && slot == 0);
+    }
+
+    @Override
+    public boolean canExtract(int slot, ItemStack stack, Direction side) {
+        return side == Direction.DOWN && slot == 2;
+    }
 
     private void resetProgress() {
         this.progress = 0;
     }
 
     public static void tick(World world, BlockPos blockPos, BlockState state, CocktailFrozeBlockEntity entity) {
+        ItemStack itemStack = entity.inventory.get(1);
+
         if(world.isClient()) {
             return;
         }
+        if(!entity.isFrozing()){
+            entity.frozeTime = entity.getFrozeTime(itemStack);
+        }
 
-        if(hasRecipe(entity)){
+        if(hasRecipe(entity) && entity.isFrozing()){
             entity.progress++;
             markDirty(world, blockPos, state);
             if(entity.progress >= entity.maxProgress){
@@ -112,6 +161,14 @@ public class CocktailFrozeBlockEntity extends BlockEntity implements NamedScreen
             markDirty(world, blockPos, state);
         }
 
+    }
+
+    protected int getFrozeTime(ItemStack fuel) {
+        if (fuel.isEmpty()) {
+            return 0;
+        }
+        Item item = fuel.getItem();
+        return CocktailFrozeBlockEntity.createFuelTimeMap().getOrDefault(item, 0);
     }
 
     private static void craftItem(CocktailFrozeBlockEntity entity) {
